a8b4a69fca16b1ee512cee4b5de5c11bbd66ab04
Anastasia Sidarovich <mc.sidr@yandex.by>
2023-05-11 16:54:16 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Anastasiya Sidarovich on 11.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
